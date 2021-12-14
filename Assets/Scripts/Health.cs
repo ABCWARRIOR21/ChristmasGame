142f156a2487d1cc6a8ddfa33650c1f6edf7c789
GalacticKatAlpha <524721@chrome.escg.ac.uk>
2021-12-14 11:50:14 +0000

    Christmas

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float startingHealth;
+    public float currentHealth;
+    private Vector3 respawnPoint;
+
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+        respawnPoint = transform.position;
+    }
+    public void TakeDamage(float _damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Checkpoint")
+        {
+            respawnPoint = transform.position;
+        }
+
+
+
+        if (currentHealth < 0.01)
+        {
+            transform.position = respawnPoint;
+        }
+
+    }
+}
